48bc22648cddb6ba5cd1db63c506f6748e0e76be
Wasie123 <127023454+rens20@users.noreply.github.com>
2023-08-23 00:40:41 +0800

    Update createaccount.js

--- createaccount.js
+++ createaccount.js
@@ -1,59 +1,53 @@
 const makePostRequest = async (data, endpoint, callback) => {
-const sent = await fetch(endpoint, {
-  method: 'POST',
+  const sent = await fetch(endpoint, {
+      method: 'POST',
       headers: {
-        'Content-Type': 'application/json'
+          'Content-Type': 'application/json'
       },
       body: JSON.stringify(data)
-  })
+  });
   try {
-    const response = await sent.json()
-      callback(response)
+      const response = await sent.json();
+      callback(response);
   } catch (error) {
-      console.log(error)
-      callback(error)
+      console.log(error);
+      callback(error);
   }
-}
-
-function btnLogin(){
-userame = document.getElementsByName('username')[0].value;
-name = document.getElementsByName('name')[0].value;
-lastname = document.getElementsByName('lastname')[0].value;
-telephone =document.getElementsByName('telephone')[0].value;
-email = document.getElementsByName('email')[0].value;
-passwod = document.getElementsByName('password')[0].value;
-makePostRequest( { username,name,lastname,telephone,email, password }, 'https://nodejs-1.wasieacuna.repl.co/api/login', (msg) => {
-  if(msg.code === 200) {
-    alert("Logged in success!");
-    localStorage.setItem('token', msg.token)
-    window.location.href='login.html';
-  } else {
-    alert('create account failed')
-  }
-})
 };
 
+document.addEventListener('DOMContentLoaded', function() {
+  const btnCreate = document.getElementById('btnCreate');
 
-document.addEventListener("DOMContentLoaded", function () {
-  const createButton = document.getElementById("btnLogin");
+  btnCreate.addEventListener('click', async function() {
+      const form = document.getElementById('registrationForm');
+      const username = form.username.value;
+      const name = form.name.value;
+      const lastname = form.lastname.value;
+      const telephone = form.telephone.value;
+      const email = form.email.value;
+      const password = form.password.value;
 
-  const makePostRequest = async (data, endpoint, callback) => {
+      if (username && name && lastname && telephone && email && password) {
+          const registrationData = {
+              username,
+              name,
+              lastname,
+              telephone,
+              email,
+              password
+          };
 
-
-
-    const sent = await fetch(endpoint, {
-      method: 'POST',
-          headers: {
-            'Content-Type': 'application/json'
-          },
-          body: JSON.stringify(data)
-      })
-      try {
-        const response = await sent.json()
-          callback(response)
-      } catch (error) {
-          console.log(error)
-          callback(error)
+          makePostRequest(registrationData, 'https://your-api-endpoint/register', (response) => {
+              if (response.success) {
+                  alert('User registered successfully');
+                  // Redirect to a confirmation page or login page
+                  window.location.href = 'confirmation.html';
+              } else {
+                  alert('Registration failed');
+              }
+          });
+      } else {
+          alert('Please fill out all fields');
       }
-    }
   });
+});
