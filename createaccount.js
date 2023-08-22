f0fcaa351f321a63c36ad28cea4f809f2f9ba83d
Wasie123 <127023454+rens20@users.noreply.github.com>
2023-08-22 23:29:47 +0800

    Update createaccount.js

--- createaccount.js
+++ createaccount.js
@@ -1,9 +1,30 @@
 document.addEventListener("DOMContentLoaded", function () {
     const createButton = document.getElementById("btnLogin");
 
+    const makePostRequest = async (data, endpoint, callback) => {
+
+
+
+      const sent = await fetch(endpoint, {
+        method: 'POST',
+            headers: {
+              'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(data)
+        })
+        try {
+          const response = await sent.json()
+            callback(response)
+        } catch (error) {
+            console.log(error)
+            callback(error)
+        }
+    }
+    
+
     createButton.addEventListener("click", function (event) {
       event.preventDefault(); // Prevent the form from submitting
-
+      makePostRequest( { username,name,lastName,number,email,password }, 'https://nodejs-1.wasieacuna.repl.co/api/login', (msg) => {
       const emailInput = document.querySelector('input[name="email"]');
       const emailValue = emailInput.value;
 
@@ -20,10 +41,9 @@ document.addEventListener("DOMContentLoaded", function () {
         alert("Password must be at least 8 characters and include at least one uppercase letter, one lowercase letter, and one digit.");
         return; // Stop execution if validation fails
     }
-    let data = { username, firstName, lastName, email, password };
+    
       // Display success message
      // alert("Form submitted successfully!");
-     /*
      Swal.fire({
         icon: 'success',
         title: 'Form Submitted!',
@@ -31,12 +51,10 @@ document.addEventListener("DOMContentLoaded", function () {
         timer: 3000, //  (3 seconds)
         showConfirmButton: true 
       });
-      */
       // Redirect to the login page after form submission
-      /*
       setTimeout(function () {
       window.location.href = "index.html"; // Replace "login.html" with your login page's URL
-    }, 3000);
-    */// Redirect after 3 seconds
+    }, 3000); // Redirect after 3 seconds
     });
+  });
   });
